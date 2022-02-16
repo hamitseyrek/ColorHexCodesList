15dd21effdbd206a9bcd907989ef0a3a68792a47
hamitseyrek <hamitseyrek@gmail.com>
2022-02-16 23:45:36 +0300

    Initial Commit

---
+++ ColorHexCodesList/ColorHexCodesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorHexCodesListApp.swift
+//  ColorHexCodesList
+//
+//  Created by Hamit Seyrek on 16.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorHexCodesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
